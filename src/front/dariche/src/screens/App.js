42360252cff5df89cac02174b8ffd440b43f611e
Farbod Shahinfar <fshahinfar1@gmail.com>
2020-01-21 08:55:39 +0330

    Create react app

---
+++ src/front/dariche/src/screens/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+		<div>
+			<h1>Dariche</h1>
+			<input type="file" id="file-input" />
+		</div>
+  );
+}
+
+export default App;
